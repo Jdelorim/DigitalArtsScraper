a73b17f20046486eb24a458e793bc4bcbebb5296
Jdelorim <joshuadelorimier@gmail.com>
2018-09-19 13:31:58 -0400

    progress

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     console.log('hi');
     $(".sidenav").sidenav();
+   
 
 
 });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,7 +5,12 @@ module.exports = function(app) {
     
     app.get("/", function (req,res) {
       // res.sendFile(path.join(__dirname, "../public/index.html"));
-      res.render("index",{ title:"Digital Arts Scraper" });
+      res.render("index",{ 
+          title: "Digital Arts Scraper", 
+          headline: "How Framestore created the 60s-style spaceship in Black Mirror Season 4's 'USS Calister'",
+          blurb: "The VFX house created the USS Callister, the Arachnajax beast and space environments for the Emmy-winning episode."
+          
+        });
 
     });
     
