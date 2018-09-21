76dfe467da2b3876f93e3e0650178e6ff649f598
Jdelorim <joshuadelorimier@gmail.com>
2018-09-21 11:39:35 -0400

    readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # DigitalArtsScraper
+
+**About DigitalArtsScraper**
+Digital Arts Scraper, Scrapes all the articles from [Digital Arts Online](https://www.digitalartsonline.co.uk/news/). Then lets you save and comment on your favorite articles!
+Check out the deployed Project at [https://digitalartsscraper.herokuapp.com/](https://digitalartsscraper.herokuapp.com/)
+
+**Author**
+- Created by Joshua deLorimier
+- September 2018
+
+**Technologies Used**
+- node.js
+- express
+- axios
+- cheerio
+- handlebars
+- mongodb
+- moongoogse
+- materalize
--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,40 +1,31 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $(".sidenav").sidenav();
 
-/*
-    $(".checkArticles").each(function() {
-    if ($(this).attr("data-target") !== true ) {
-    $(this).show().children().show();
-    console.log(this);
-    } else {
-        $(this).hide();
-    }
-});
-    */
-   $(".checkArticles").each(function() {
-       var target = $(this).attr("data-target");
-       // console.log("target:", target );
-        if(target === "true") {
+
+    $(".checkArticles").each(function () {
+        var target = $(this).attr("data-target");
+        // console.log("target:", target );
+        if (target === "true") {
             $(this).show().children().show();
         } else {
             $(this).hide();
         }
-});
-    
-    $(".saveBtn").on("click", function() {
+    });
+
+    $(".saveBtn").on("click", function () {
         var storeID = $(this).attr("data-target");
         console.log("storeID:", storeID);
-        
-    $.ajax({
-        method: "POST",
-        url: "/saveID",
-        data: {
-             body: storeID
-          }
-      })
-      .catch(function(err) {
-        console.log(err);
-      })
+
+        $.ajax({
+                method: "POST",
+                url: "/saveID",
+                data: {
+                    body: storeID
+                }
+            })
+            .catch(function (err) {
+                console.log(err);
+            })
 
     });
 
